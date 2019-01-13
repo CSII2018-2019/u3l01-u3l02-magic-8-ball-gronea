e2a69285abda6aef2dcfc3937c166890b1f31d73
gronea <gronea@LR064W7M-1718.ellis.int>
2019-01-13 16:02:53 -0500

    This is the layout to the magic Eight Ball.

--- src/MagicEightBall.java
+++ src/MagicEightBall.java
@@ -1,9 +1,97 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
 
-public class MagicEightBall {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+public class MagicEightBall extends JFrame{
+	
+	public MagicEightBall () {
+		//setup what goes into the window
+		initGUI();
+		//Title
+		setTitle("Online Magic Eight Ball");
+		//Size
+		setSize(800, 350); //pixels
+		setResizable(true);
+		//Makes everything really tight - overrides size
+		//pack();
+		setLocationRelativeTo(null); //centers on screen, do this after packing but before visible
+
+		//Needs to one of the last things you do- making it visible	
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void initGUI() {
+		//TITLE
+		//create label
+		JLabel titleLabel = new JLabel("Magic Eight Ball");
+		//Customizing label
+		//Extra Font Options are on the slide "100 Fonts"
+		//Once Downloaded replace Font.SERIF with "[FONT NAME]"
+		Font titleFont = new Font (Font.SANS_SERIF, Font.BOLD, 32);
+		titleLabel.setFont(titleFont);
+		titleLabel.setForeground(Color.LIGHT_GRAY);
+		//titleLabel.setBackground(Color.BLACK); 
+		//or Go on Google and search rgb color picker to find new colors then use:
+		titleLabel.setBackground(new Color(29,62,114));
+		titleLabel.setOpaque(true);
+		//Add the label to the window, make sure it is on top
+		add(titleLabel, BorderLayout.PAGE_START);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER); //left or right
+		
+		//CENTER PANEL
+		JPanel centerPanelBackground = new JPanel ();
+		centerPanelBackground.setBackground(Color.BLACK);
+		//add random array for shake
+		add(centerPanelBackground, BorderLayout.CENTER);
+		
+		Dimension size = new Dimension(400,200);
+		
+		JPanel centerPanel = new JPanel ();
+		centerPanel.setBackground(Color.WHITE); //This will change with buttons
+		centerPanel.setPreferredSize(size);
+		centerPanelBackground.add(centerPanel);
+		
+		JLabel text = new JLabel ("Welcome"); //"Text" will become a variable to change
+		Font textFont = new Font (Font.SANS_SERIF, Font.BOLD + Font.ITALIC, 16);
+		text.setFont(textFont);
+		text.setForeground(Color.BLUE);
+		centerPanel.add(text);
+		text.setHorizontalAlignment(JLabel.CENTER);
+		
+		//BUTTON PANEL
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBackground(new Color(29,62,114));
+		add(buttonPanel, BorderLayout.PAGE_END);
+		
+		//Buttons
+		JButton shakeButton = new JButton("Shake"); //Add code to make it change text
+		shakeButton.setBackground(Color.WHITE);
+		buttonPanel.add(shakeButton);
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+            //UI = user interface
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+            UIManager.setLookAndFeel(className);
+        } catch ( Exception e) {}
+        
+        EventQueue.invokeLater(new Runnable (){
+            @Override
+            public void run() {
+                new MagicEightBall();
+            }   
+        });
 	}
 
 }
